19b5aed6f9656cf9ee30f9b064da4e3db5481497
KBluePurple <42506968+BlWon@users.noreply.github.com>
2022-05-27 12:00:46 +0900

    완료

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,22 +8,28 @@ public class PlayerMove : MonoBehaviour
     private Vector3 _rotate;
 
     [SerializeField] float _speed = 1f;
+    [SerializeField] float _rotateSpeed = 1f;
+    [SerializeField] float _speedChange = 1f;
     [SerializeField] float _angleSmooth= 3f;
+    [SerializeField] float _maxSpeed = 10f;
     [SerializeField] Image visual;
 
     private void Update()
     {
         float horizontal = Input.GetAxis("Mouse X");
         float vertical = Input.GetAxis("Mouse Y");
+        float rotate = Input.GetAxis("Horizontal");
 
         horizontal = Mathf.Clamp(horizontal, -1f, 1f);
         vertical = Mathf.Clamp(vertical, -1f, 1f);
 
-        Vector3 lerpVector = new Vector3(-vertical, horizontal, 0).normalized * ((Mathf.Abs(horizontal) + Mathf.Abs(vertical)) / 2);
+        Vector3 lerpVector = new Vector3(vertical, horizontal, 0).normalized * ((Mathf.Abs(horizontal) + Mathf.Abs(vertical)) / 2);
+        lerpVector += new Vector3(0, 0, rotate);
         _rotate = Vector3.Lerp(_rotate, lerpVector, _angleSmooth * Time.deltaTime);
-        visual.rectTransform.anchoredPosition = new Vector2(_rotate.y, -_rotate.x) * 100;
-
-        transform.Rotate(_rotate);
+        transform.Rotate(_rotate * _rotateSpeed * _speedChange);
+        visual.rectTransform.anchoredPosition = new Vector2(_rotate.y, _rotate.x) * 100;
+ 
+        _speed += Input.GetAxis("Vertical") * _speedChange * Time.deltaTime;
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
     }
 }
