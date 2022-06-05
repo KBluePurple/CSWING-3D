772d38079a25b53564a8b7da48d5883e47bf7517
냠냠 <lanilani7661@gmail.com>
2022-06-05 17:51:43 +0900

    기체 개조 창 추가

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -7,20 +7,46 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour
 {
     [SerializeField]
+    private GameObject title;
+    [SerializeField]
     private GameObject setting;
     [SerializeField]
     private GameObject selectGameMode;
     [SerializeField]
-    private GameObject title;
+    private GameObject repair;
+
+    [Header("Scroll")]
+    [SerializeField]
+    private GameObject remodelScroll;
+    [SerializeField]
+    private GameObject spaceshipScroll;
+    [SerializeField]
+    private GameObject powerScroll;
 
     //private void Start()
     //{
     //    GetComponent<Button>().onClick.AddListener(() => { });
     //}
 
+    public void Back()
+    {
+        selectGameMode.SetActive(false);
+        repair.SetActive(false);
+        setting.SetActive(false);
+        title.SetActive(true);
+    }
+
     public void OnStart()
     {
+        Debug.Log("SelectGameMode");
         selectGameMode.SetActive(true);
+        title.SetActive(false);
+    }
+
+    public void OnRepair()
+    {
+        Debug.Log("Repair");
+        repair.SetActive(true);
         title.SetActive(false);
     }
 
@@ -30,20 +56,35 @@ public class ButtonManager : MonoBehaviour
         SceneManager.LoadScene("GameScene");
     }
 
-    public void BodySetting()
+    public void DisalbleAllScroll()
     {
-        Debug.Log("BodySetting");
+        remodelScroll.SetActive(false);
+        spaceshipScroll.SetActive(false);
+        powerScroll.SetActive(false);
+    }
+
+    public void ActiveRemodelScroll()
+    {
+        DisalbleAllScroll();
+        remodelScroll.SetActive(true);
+    }
+
+    public void ActiveSpaceShipScroll()
+    {
+        DisalbleAllScroll();
+        spaceshipScroll.SetActive(true);
+    }
+
+    public void ActivePowerScroll()
+    {
+        DisalbleAllScroll();
+        powerScroll.SetActive(true);
     }
 
     public void OnSetting()
     {
         setting.SetActive(true);
         //Debug.Log("Setting");
-    }
-
-    public void OffSetting()
-    {
-        setting.SetActive(false);
     }
 
     public void OnQuit()
