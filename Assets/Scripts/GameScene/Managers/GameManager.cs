cdca58db0581602be756ca18de88e97e5d8eddb2
냠냠 <lanilani7661@gmail.com>
2022-06-09 19:20:34 +0900

    적 스폰 수정

--- Assets/Scripts/GameScene/Managers/GameManager.cs
+++ Assets/Scripts/GameScene/Managers/GameManager.cs
@@ -12,8 +12,14 @@ namespace GameScene
         private int maxPursuitSpawnCount = 5;
 
         private int curPursuitSpawnCount = 0;
-        private float spawnDelay = 10f;
+        private float spawnDelay = 90f;
         private float curDelay = 0f;
+
+        private int curFixedPursuitSpawnCount = 3;
+        private int curMovedPursuitSpawnCount = 2;
+
+        [SerializeField]
+        private GameObject player;
 
         void Start()
         {
@@ -25,20 +31,47 @@ namespace GameScene
         {
             curDelay += Time.deltaTime;
 
-            if (curDelay >= spawnDelay && curPursuitSpawnCount < maxPursuitSpawnCount)
-            {
-                SpawnPursuit();
-                curDelay = 0f;
-                Debug.Log("��ȯ");
-                curPursuitSpawnCount++;
-            }
+            SpawnPurusit();
         }
 
-        void SpawnPursuit()
+
+        void SpawnPurusit()
         {
-            float spawnPosX = Random.Range(-450f, 450f);
-            float spawnPosY = Random.Range(-450f, 450f);
-            float spawnPosZ = Random.Range(-450f, 450f);
+            if (curPursuitSpawnCount == 0 || curDelay >= spawnDelay)
+            {
+                if (curPursuitSpawnCount <= curFixedPursuitSpawnCount)
+                {
+                    SpawnFixedPursuit();
+                    curPursuitSpawnCount++;
+                    curDelay = 0f;
+                    Debug.Log("고정 적 생성");
+                }
+                else if (curPursuitSpawnCount > curFixedPursuitSpawnCount &&
+                        curFixedPursuitSpawnCount + curMovedPursuitSpawnCount <= curPursuitSpawnCount)
+                {
+                    SpawnMovedPursuit();
+                    curPursuitSpawnCount++;
+                    curDelay = 0f;
+                    Debug.Log("움직이는 적 생성");
+                }
+            }
+        }
+        void SpawnFixedPursuit()
+        {
+            float spawnPosX = Random.Range(player.transform.position.x + 50f, player.transform.position.x + 450f);
+            float spawnPosY = Random.Range(player.transform.position.y + 50f, player.transform.position.y + 450f);
+            float spawnPosZ = Random.Range(player.transform.position.z + 50f, player.transform.position.z + 450f);
+
+            Vector3 spawnPos = new Vector3(spawnPosX, spawnPosY, spawnPosZ);
+
+            Instantiate(pursuitPre, spawnPos, Quaternion.identity);
+        }
+
+        void SpawnMovedPursuit()
+        {
+            float spawnPosX = Random.Range(player.transform.position.x - 50f, player.transform.position.x + 50f);
+            float spawnPosY = Random.Range(player.transform.position.y - 50f, player.transform.position.y + 450f);
+            float spawnPosZ = Random.Range(player.transform.position.z - 50f, player.transform.position.z + 450f);
 
             Vector3 spawnPos = new Vector3(spawnPosX, spawnPosY, spawnPosZ);
 
