9e2b7910e7cdb0f45b666eb13ad69c218d60a39b
hun1001 <l01022899266@gmail.com>
2022-06-10 00:19:44 +0900

    베이스 제작 후 추가

---
+++ Assets/Scripts/TitleScene/Parts/Base/WeaponBase.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBase : MonoBehaviour
+{
+    protected float _atk;
+    protected float _distance;
+    protected BulletType _bulletType;
+}
