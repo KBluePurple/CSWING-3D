60d539efde28ad206d6d0ca2aa918331acbe0062
hun1001 <l01022899266@gmail.com>
2022-06-10 11:11:27 +0900

    안보이게 수정 리페어 뒤로가기 버튼

---
+++ Assets/Scripts/TitleScene/Manager/RepairUIManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairUIManager : MonoSingleton<RepairUIManager>
+{
+    [SerializeField]
+    private GameObject _backButton = null;
+
+    private int _titlePageNum;
+
+    private void Start()
+    {
+        RepairPageChange(0);
+    }
+
+    public void RepairPageChange(int pageNum)
+    {
+        _titlePageNum = pageNum;
+        if (_titlePageNum == 0)
+        {
+            _backButton.SetActive(false);
+        }
+        else
+        {
+            _backButton.SetActive(true);
+        }
+    }
+}
