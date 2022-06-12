e114e9c300fff6339e79f0df7970b413ebcc7d87
KBluePurple <42506968+BlWon@users.noreply.github.com>
2022-06-13 00:44:26 +0900

    enum 채워줘

--- Assets/Scripts/TitleScene/PartsSetting.cs
+++ Assets/Scripts/TitleScene/PartsSetting.cs
@@ -4,8 +4,40 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+public enum CorePart
+{
+
+}
+
+public enum EnginePart
+{
+
+}
+
+public enum BodyPart
+{
+
+}
+
+public enum WeaponPart
+{
+
+}
+
+public enum WeaponType
+{
+    Main,
+    Sub
+}
+
 public class PartsSetting : MonoBehaviour
 {
+    public static CorePart Core;
+    public static EnginePart Engine;
+    public static BodyPart Body;
+    public static WeaponPart Weapon;
+    public static WeaponPart SubWeapon;
+
     [SerializeField]
     private Image _pCore;
     [SerializeField]
@@ -13,13 +45,9 @@ public class PartsSetting : MonoBehaviour
     [SerializeField]
     private Image _pBody;
     [SerializeField]
-    private Image _pWeapon1;
+    private Image _pWeapon;
     [SerializeField]
-    private Image _pWeapon2;
-    [SerializeField]
-    private Image _pSweapon1;
-    [SerializeField]
-    private Image _pSweapon2;
+    private Image _pSweapon;
 
     public void GetPart(Image image, PartType type)
     {
@@ -34,18 +62,44 @@ public class PartsSetting : MonoBehaviour
             case PartType.Body:
                 _pBody.sprite = image.sprite;
                 break;
-            case PartType.Weapon1:
-                _pWeapon1.sprite = image.sprite;
+            case PartType.Weapon:
+                _pWeapon.sprite = image.sprite;
                 break;
-            case PartType.Weapon2:
-                _pWeapon2.sprite = image.sprite;
+            case PartType.SWeapon:
+                _pSweapon.sprite = image.sprite;
                 break;
-            case PartType.SWeapon1:
-                _pSweapon1.sprite = image.sprite;
-                break;
-            case PartType.SWeapon2:
-                _pSweapon2.sprite = image.sprite;
-                break;
+        }
+    }
+
+    public void SetPart(Image image, CorePart core)
+    {
+        _pCore.sprite = image.sprite;
+        Core = core;
+    }
+
+    public void SetPart(Image image, EnginePart engine)
+    {
+        _pBooster.sprite = image.sprite;
+        Engine = engine;
+    }
+
+    public void SetPart(Image image, BodyPart body)
+    {
+        _pBody.sprite = image.sprite;
+        Body = body;
+    }
+
+    public void SetPart(Image image, WeaponPart weapon, WeaponType type)
+    {
+        if(type == WeaponType.Main)
+        {
+            _pWeapon.sprite = image.sprite;
+            Weapon = weapon;
+        }
+        else
+        {
+            _pSweapon.sprite = image.sprite;
+            SubWeapon = weapon;
         }
     }
 
@@ -62,17 +116,11 @@ public class PartsSetting : MonoBehaviour
             case PartType.Body:
                 _pBody.sprite = null;
                 break;
-            case PartType.Weapon1:
-                _pWeapon1.sprite = null;
+            case PartType.Weapon:
+                _pWeapon.sprite = null;
                 break;
-            case PartType.Weapon2:
-                _pWeapon2.sprite = null;
-                break;
-            case PartType.SWeapon1:
-                _pSweapon1.sprite = null;
-                break;
-            case PartType.SWeapon2:
-                _pSweapon2.sprite = null;
+            case PartType.SWeapon:
+                _pSweapon.sprite = null;
                 break;
         }
     }
