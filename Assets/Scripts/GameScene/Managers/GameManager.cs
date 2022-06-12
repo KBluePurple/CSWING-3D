c0783618a92ccca5d1cb6b30a43d9b5edaf64985
hun1001 <l01022899266@gmail.com>
2022-06-12 12:45:43 +0900

    Update GameManager.cs

--- Assets/Scripts/GameScene/Managers/GameManager.cs
+++ Assets/Scripts/GameScene/Managers/GameManager.cs
@@ -6,10 +6,44 @@ namespace GameScene
 {
     public class GameManager : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject pursuitPre;
+        [SerializeField]
+        private int maxPursuitSpawnCount = 5;
+        public GameObject Player;
+
+        private int curPursuitSpawnCount = 0;
+        private float spawnDelay = 10f;
+        private float curDelay = 0f;
+
         void Start()
         {
             MouseManager.Show(false);
             MouseManager.Lock(true);
+        }
+
+        private void Update()
+        {
+            curDelay += Time.deltaTime;
+
+            if (curDelay >= spawnDelay && curPursuitSpawnCount < maxPursuitSpawnCount)
+            {
+                SpawnPursuit();
+                curDelay = 0f;
+                Debug.Log("소환");
+                curPursuitSpawnCount++;
+            }
+        }
+
+        void SpawnPursuit()
+        {
+            float spawnPosX = Random.Range(-450f, 450f);
+            float spawnPosY = Random.Range(-450f, 450f);
+            float spawnPosZ = Random.Range(-450f, 450f);
+
+            Vector3 spawnPos = new Vector3(spawnPosX, spawnPosY, spawnPosZ);
+
+            Instantiate(pursuitPre, spawnPos, Quaternion.identity);
         }
 
     }
