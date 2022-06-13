5044686722e015100f446ed60c1fbdc8a7dc0faa
냠냠 <lanilani7661@gmail.com>
2022-06-13 23:47:41 +0900

    레벨매니저 스크립트 생성

--- Assets/Scripts/GameScene/Enemy/SurvivorMode Level/Level5.cs
+++ Assets/Scripts/GameScene/Enemy/SurvivorMode Level/Level5.cs
@@ -6,11 +6,13 @@ public class Level5 : MonoBehaviour
 {
     [SerializeField]
     private int subBoss = 1;
+    [SerializeField]
+    private int subBossHP = 150;
 
     [SerializeField]
     private GameObject subBossPre;
 
-    private void SpawnFixedPursuit()
+    private void SpawnMovedPursuit()
     {
         Vector3 spawnPos = subBossPre.transform.position;
 
