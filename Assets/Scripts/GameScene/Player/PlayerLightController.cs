a2214b4864691fec819eac994db5e514e1351668
hun1001 <l01022899266@gmail.com>
2022-06-16 12:46:37 +0900

    이펙트 하나 추가

---
+++ Assets/Scripts/GameScene/Player/PlayerLightController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameScene;
+
+public class PlayerLightController : MonoBehaviour
+{
+    private Light[] _lights;
+
+    void Start()
+    {
+        _lights = GetComponentsInChildren<Light>();
+    }
+
+    void Update()
+    {
+        foreach (var light in _lights)
+        {
+            light.intensity = Mathf.Lerp(light.intensity, PlayerMove.Instance._speed / 10, Time.deltaTime);
+            light.intensity = Mathf.Clamp(light.intensity, 1, 10);
+        }
+    }
+}
