e555a616b21a94bc146b167052590354418390b2
zmflqhaos <zmflqhaos123@gmail.com>
2022-06-18 00:22:54 +0900

    loadStats

--- Assets/Scripts/GameScene/Managers/PlayerManager.cs
+++ Assets/Scripts/GameScene/Managers/PlayerManager.cs
@@ -14,17 +14,7 @@ namespace GameScene
         //특수스킬1, 특수스킬2
         private GameObject playerSkill1;
         private GameObject playerSkill2;
-        //최고속도
-        private float maxSpeed;
-        //현재속도
-        private float curSpeed;
-        //가속도
-        private float playerAcceleration;
-        //감속도
-        private float deceleration;
-        //부스트 속도, 쿨타임
-        private float boostSpeed;
-        private float boostCooltime;
+
         //몸체 회전속도, 감도
         private float rotationSpeed;
         //감도? 는 어케? 쓰?지?
@@ -38,7 +28,7 @@ namespace GameScene
 
         private void Start()
         {
-            Stat = new PlayerStat(100, 200, 100);
+            Stat = new PlayerStat();
             uiManager = FindObjectOfType<UIManager>();
         }
 
--- Assets/Scripts/GameScene/Player/PlayerStat.cs
+++ Assets/Scripts/GameScene/Player/PlayerStat.cs
@@ -7,20 +7,31 @@ namespace GameScene
 {
     public class PlayerStat
     {
-        public PlayerStat(int hp, int shield, int energy)
+        public PlayerStat()
         {
-
-            _maxHp = _hp = hp;
-            _maxShield = _shield = shield;
-            _maxEnergy = _energy = energy;
+            CallAllParts();
         }
 
-        private int _maxHp = 100;
-        private int _maxShield = 200;
-        private int _maxEnergy = 100;
-        private int _hp = 100;
-        private int _shield = 200;
-        private int _energy = 100;
+        private int _maxHp;
+        private int _maxShield;
+        private int _maxEnergy;
+        private int _hp;
+        private int _shield;
+        private int _energy;
+        //최고속도
+        private float maxSpeed;
+        //현재속도
+        private float curSpeed;
+        //가속도
+        private float playerAcceleration;
+        //감속도
+        private float deceleration;
+        //부스트 속도, 쿨타임
+        private float boostSpeed;
+        private float boostCooltime;
+        private CorePart core;
+        private BodyPart body;
+        private EnginePart engine;
         public int Hp
         {
             get
@@ -82,6 +93,64 @@ namespace GameScene
                 }
                 UIManager.Instance.Energy.fillAmount = _energy / (float)_maxEnergy;
             }
+        }
+
+        private void CallAllParts()
+        {
+            core = SaveManager.Instance.Parts.Core;
+            body = SaveManager.Instance.Parts.Body;
+            engine = SaveManager.Instance.Parts.Engine;
+            switch(core)
+            {
+                case CorePart.CORE_01 :
+                    _maxShield = 100;
+                    break;
+                case CorePart.CORE_02:
+                    _maxShield = 150;
+                    break;
+                case CorePart.CORE_03:
+                    _maxShield = 75;
+                    break;
+            }
+            switch (body)
+            {
+                case BodyPart.CS_01:
+                    _maxHp = 200;
+                    break;
+                case BodyPart.CS_02:
+                    _maxHp = 250;
+                    break;
+                case BodyPart.CS_03:
+                    _maxHp = 150;
+                    break;
+            }
+            switch (engine)
+            {
+                case EnginePart.BU_01:
+                    maxSpeed = 50;
+                    playerAcceleration = 10;
+                    deceleration = 8;
+                    boostSpeed = 10;
+                    boostCooltime = 5;
+                    break;
+                case EnginePart.BU_02:
+                    maxSpeed = 75;
+                    playerAcceleration = 15;
+                    deceleration = 10;
+                    boostSpeed = 20;
+                    boostCooltime = 10;
+                    break;
+                case EnginePart.BU_03:
+                    maxSpeed = 35;
+                    playerAcceleration = 10;
+                    deceleration = 8;
+                    boostSpeed = 10;
+                    boostCooltime = 3;
+                    break;
+            }
+            _shield = _maxShield;
+            _hp = _maxHp;
+            curSpeed = 0;
         }
     }
 }
