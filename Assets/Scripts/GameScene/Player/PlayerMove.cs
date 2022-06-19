6496715c317e16e6653af32b17ccf0eaf409c311
KBluePurple <42506968+BlWon@users.noreply.github.com>
2022-06-20 00:14:33 +0900

    아마됨

--- Assets/Scripts/GameScene/Player/PlayerMove.cs
+++ Assets/Scripts/GameScene/Player/PlayerMove.cs
@@ -164,9 +164,6 @@ namespace GameScene
             GameObject effect = Instantiate(explosionEffect, transform.position, Quaternion.identity);
             yield return new WaitForSeconds(2f);
             Destroy(effect);
-            PlayerManager.Instance.Damaged(10);
-
-            //Destroy(gameObject);
         }
     }
 }
